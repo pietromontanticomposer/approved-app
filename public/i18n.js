6ae1bc5afcb5e057ccd246d8349c217392cf5cc2
pietromontanticomposer <pietromontanticomposer@gmail.com>
2026-03-12 23:10:04 +0100

    Adjust leave project label

--- public/i18n.js
+++ public/i18n.js
@@ -449,7 +449,7 @@
       'modal.addNotePlaceholder': 'Scrivi una nota per il team...',
 
       // Actions
-      'project.leave': 'Lascia progetto',
+      'project.leave': 'lascia progetto',
       'project.leaveConfirm': 'Lasciare "{name}"? Perderai l\'accesso a questo progetto.',
       'project.leaveTeamShared': 'Questo progetto ti e\' condiviso tramite un team. Devi uscire dal team per perdere l\'accesso.',
       'action.rename': 'Rinomina',
